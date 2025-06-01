bd4ce81c2c84cdc960d6c8c1f37bc481071545c6
KatarzynaGawinecka <katarzyna.gawinecka.936@studms.ug.edu.pl>
2025-06-01 23:58:12 +0200

    koniec rekurencji

---
+++ Zadanie 1 (rekurencja)/Zadanie 1 (rekurencja)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1__rekurencja_
+{
+    class Program
+    {
+        static long silnia(int n)
+        {
+            //Napisz funkcję , która oblicza wartość silni z zadanej przez argument wywołania funkcji 
+            //  wartości liczby naturalnej.Utwórz wersję rekurencyjną tej funkcji.
+            //Napisz program testowy wykorzystujący napisaną funkcję.
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
+        static void Main(string[]args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny argument");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
+        }
+    }
+}
